c7c526da929bbc66acc1b438c39e07e92dd53527
ABDIEL ANTONIO CORRO <aacorro@outlook.com>
2023-06-20 22:52:22 -0500

    connection string

--- Bulky.DataAccess/DbInitializer/DbInitializer.cs
+++ Bulky.DataAccess/DbInitializer/DbInitializer.cs
@@ -49,14 +49,14 @@ namespace BulkyBook.DataAccess.DbInitializer {
 
                 //if roles are not created, then we will create admin user as well
                 _userManager.CreateAsync(new ApplicationUser {
-                    UserName = "admin@dotnetmastery.com",
-                    Email = "admin@dotnetmastery.com",
-                    Name = "Bhrugen Patel",
+                    UserName = "ACorro",
+                    Email = "acorrop@icloud.com",
+                    Name = "Acorro",
                     PhoneNumber = "1112223333",
                     StreetAddress = "test 123 Ave",
-                    State = "IL",
-                    PostalCode = "23422",
-                    City = "Chicago"
+                    State = "WI",
+                    PostalCode = "54303",
+                    City = "Wisconsin"
                 }, "Admin123*").GetAwaiter().GetResult();
 
 
